65143aa42dd3f5477f4503ffcaffcc0cfb731efd
jh <dev1@correo.com>
2019-10-11 10:43:51 -0500

    cambios en format e index

--- src/public/js/format.js
+++ src/public/js/format.js
@@ -1,14 +1,26 @@
 'use strict'
 
 const data = document.getElementById('comments-text').textContent
+const descriptionData = document.getElementById('description-text').textContent
+
 const separador = '.'
   
 const newArray = data.split(separador)
 const dataText = document.getElementById('comments-text')
+
+const newArrayDescription = descriptionData.split(separador)
+const descriptionDataText = document.getElementById('description-text')
   
 document.getElementById('comments-text').textContent = ''
 newArray.map((el, i)=>{
   dataText.innerHTML += `
+    <li>${el}.</li>
+  `
+})
+
+document.getElementById('description-text').textContent = ''
+newArrayDescription.map((el, i)=>{
+  descriptionDataText.innerHTML += `
     <li>${el}.</li>
   `
 })
